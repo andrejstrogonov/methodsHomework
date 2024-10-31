0d975406c0ddd2df6373c7d2e1331d85d8f7cde5
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-31 22:00:16 +0300

    Finish second task

--- src/main/java/org/andrejstrogonov/App.java
+++ src/main/java/org/andrejstrogonov/App.java
@@ -5,6 +5,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(calculatorLeapYear(2024));
+        System.out.println(calculateDeviceVersion(1,2014));
     }
 
     public static String calculatorLeapYear(int i) {
@@ -14,4 +15,19 @@ public class App
             return i + " год — невисокосный год";
         }
     }
+     public static String calculateDeviceVersion(int i, int i1) {
+         if (i == 1) {
+             if (i1 <= 2015) {
+                 return "Установите облегченную версию приложения для Android по ссылке";
+             } else {
+                 return "Установите версию приложения для Android по ссылке";
+             }
+         } else {
+             if (i1 <= 2015) {
+                 return "Установите облегченную версию приложения для iOS по ссылке";
+             } else {
+                 return "Установите версию приложения для iOS по ссылке";
+             }
+         }
+     }
 }
--- src/test/java/org/andrejstrogonov/AppTest.java
+++ src/test/java/org/andrejstrogonov/AppTest.java
@@ -17,6 +17,14 @@ public class AppTest
     {
         assertEquals("2023 год — невисокосный год",App.calculatorLeapYear(2023));
     }
+    @Test
+    public void testYearOs(){
+        assertEquals("Установите версию приложения для Android по ссылке",App.calculateDeviceVersion(1,2016));
+    }
+    @Test
+    public void testYearOsYear(){
+        assertEquals("Установите облегченную версию приложения для iOS по ссылке",App.calculateDeviceVersion(0,2013));
+    }
 
 
 }
